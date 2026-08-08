5d472f8f4fd4b56b64ad3ce1ac46a8e7e8d857a3
Genuama <87739701+Genuama@users.noreply.github.com>
2026-08-08 01:23:42 -0400

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # seen = {}
+        # for i, num in enumerate(numbers):
+        #     complement = target - num
+
+        #     if complement in seen:
+        #         return [i, seen[complement]]
+        #     seen[num] = i
+        # return []
+
+        left = 0
+        right = len(numbers) - 1
+
+        while left < right:
+            total = numbers[left] + numbers[right]
+            if total == target:
+                return [left+1, right+1]
+            elif total < target:
+                left+=1
+            else:
+                right-=1
+   
+
+        
+        
